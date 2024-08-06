d52dabe12c73a5ad84be4248beb693e3ee094fa4
Spaghetata <137336021+spaghetata@users.noreply.github.com>
2024-08-06 15:14:32 +0200

    Update get_logs.ps1

--- get_logs.ps1
+++ get_logs.ps1
@@ -60,8 +60,8 @@ function add_log {
     $get_last_id = Get-Content -Path $BASEPATH -Tail 1 | Convert-String -Example "ID | Name | Path=ID"
     $log_id = [int]$get_last_id + 1
 
-    $add_log_name = Read-Host -Prompt "Set name: "
-    $add_log_path = Read-Host -Prompt "Set path: "
+    $add_log_name = Read-Host -Prompt "Set name"
+    $add_log_path = Read-Host -Prompt "Set path"
 
     $name_path = New-Object System.Text.StringBuilder
     [void]$name_path.Append("$log_id | ")
@@ -80,10 +80,36 @@ function dellog {
     if ($ask_for_del -eq "y"){
 
         $rem_id = Read-Host "Please insert the log-id you want to delete."
-        $get_line = Get-Content -Path $BASEPATH | Select-String -Pattern "$rem_id | * | *"
-        $get_line.Replace("$rem_id | * | *","")
+
+        # checks if the input is 0
+        if ($rem_id -eq "0"){
+
+            Write-Host "Sorry but you can not delete the default entry."
+
+        }
+
+        else{
+
+            # filters the line with ID (regex)
+            $line = Get-Content -Path $BASEPATH | Select-String -Pattern "^$rem_id\b"
+            # overwrite the lib.txt without the filtered line above (so it gets deleted)
+            Get-Content -Path $BASEPATH | Where-Object { $_ -ne $line} | Set-Content -Path $BASEPATH
+
+            # changes the IDs after filtered line above
+            #$following_ids = @(Get-Content -Path $BASEPATH | Convert-String -Example "ID | Name | Path=ID" | Where-Object { [int]$_ -gt [int]$rem_id})
+            #ForEach($following_id in $following_ids){
+            #
+            #   $new_id = $following_id - 1
+            #   $update_line = Get-Content -Path $BASEPATH | Select-String -Pattern "^$following_id\b"
+            #   $update_line.replace("$following_id","$new_id")
+            #}
+
+
+
+        }
 
     }
+
 
     else{}
 
@@ -114,6 +140,8 @@ while ($var_bool) {
 
 }
 
-#TODO setup dellog
-#TODO unable to delete lib.txt entry with id 0
-#TODO when delete a entry all following ids have to be id-1
+#TODO what if dir is existing but lib.txt is not (now you get error that the dir is already existing)
+#TODO /dellog setup
+#TODO /dellog replace file is to fast (process still running)
+#TODO /dellog what if the entered id is not existing
+#TODO /dellog when delete a entry all following ids have to be id-1
